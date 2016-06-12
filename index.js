5e310aa09de9ebfc7a3615765eb75413296c4d92
liang <gaoxiaoliangz@gmail.com>
2016-06-12 23:35:01 +0800

    修复了几个代码风格问题

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('app-module-path').addPath('./assets/built/es5')
-
-var path = require('path')
-var init = require('./core/server/init')
+const path = require('path')
+const init = require('./core/server/init')
 
 module.exports = init(path.join(__dirname))
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   entry: {
     app: ['./assets/built/es6/entry/app'],
-    console: ['./assets/built/es6/entry/console']
+    console: ['./assets/built/es6/entry/console'],
     // app: ['./core/frontend/entry/app'],
     // console: ['./core/frontend/entry/console'],
   },
   output: {
     path: path.join(__dirname, 'assets/built'),
     filename: '[name].js',
-    publicPath: '/built/'
+    publicPath: '/built/',
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
-    })
+      'process.env.NODE_ENV': '"production"',
+    }),
   ],
   devServer: {
-    stats: 'errors-only'
+    stats: 'errors-only',
   },
   module: {
     loaders: [
@@ -28,30 +28,30 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['react', 'es2015']
-        }
+          presets: ['react', 'es2015'],
+        },
       },
       {
         test: /\.css?$/,
-        loaders: [ 'style', 'raw' ]
+        loaders: ['style', 'raw'],
       },
-      { 
+      {
         test: /\.tsx?$/,
         loader: 'ts-loader',
         query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
+          presets: ['react', 'es2015'],
+        },
+      },
+    ],
   },
   resolve: {
     root: path.resolve('./assets/built/es6'),
     // root: path.resolve('./core/frontend'),
     alias: {
-      vendor: path.join(__dirname, "assets/vendor"),
-      css: path.join(__dirname, "assets/built")
+      vendor: path.join(__dirname, 'assets/vendor'),
+      css: path.join(__dirname, 'assets/built'),
     },
-    extensions: ['', '.js', '.jsx', '.ts', '.tsx']
+    extensions: ['', '.js', '.jsx', '.ts', '.tsx'],
   },
   // externals: {
   //   'react': 'React',
