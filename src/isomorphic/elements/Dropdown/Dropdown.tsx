6aa4b054e01372e9b7c5da136c1d93c9ee5650aa
liang <gaoxiaoliangz@gmail.com>
2016-08-04 22:05:48 +0800

    - 解决了页面上同时用两个 dropdown 时，其中一个展开，点击另一个前者不合上的问题

--- src/isomorphic/elements/Dropdown/Dropdown.tsx
+++ src/isomorphic/elements/Dropdown/Dropdown.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import CSSModules from 'react-css-modules'
-const styles = require('./_dropdown.scss')
+const styles: any = require('./_dropdown.scss')
 
 interface IProps {
   title: string | JSX.Element
@@ -12,10 +12,24 @@ interface IState {
   showMenu?: boolean
 }
 
+// todo: 放到 utils/dom 里面
+function isDescendant(parent, child) {
+  let node = child.parentNode
+  while (node != null) {
+    if (node === parent) {
+      return true
+    }
+    node = node.parentNode
+  }
+  return false
+}
+
 @CSSModules(styles, {
   allowMultiple: true
 })
 class Dropdown extends Component<IProps, IState> {
+
+  dropdown: any
 
   constructor(props) {
     super(props)
@@ -27,16 +41,20 @@ class Dropdown extends Component<IProps, IState> {
   }
 
   toggleDropdown(e) {
-    e.stopPropagation()
     this.setState({
       showMenu: !this.state.showMenu
     })
   }
 
-  hideExpended() {
-    this.setState({
-      showMenu: false
-    })
+  hideExpended(e) {
+    // 检查点击的 dom 是否是 dropdown 的子元素
+    // 如果是那么就不做处理，而 toggleDropdown 会响应
+    // 解决了页面上同时用两个 dropdown 时，其中一个展开，点击另一个前者不合上的问题
+    if (!isDescendant(this.dropdown, e.target)) {
+      this.setState({
+        showMenu: false
+      })
+    }
   }
 
   componentDidMount() {
@@ -51,15 +69,15 @@ class Dropdown extends Component<IProps, IState> {
     const { className } = this.props
 
     return (
-      <div onClick={this.toggleDropdown} styleName="dropdown" className={className || ''}>
+      <div ref={ref => { this.dropdown = ref }} onClick={this.toggleDropdown} styleName="dropdown" className={className || ''}>
         <span styleName="dropdown-toggle">
           {this.props.title} <span styleName="dropdown-caret" />
         </span>
         <div styleName="dropdown-menu">
           <ul>
-          {
-            this.state.showMenu ? this.props.children : null
-          }
+            {
+              this.state.showMenu ? this.props.children : null
+            }
           </ul>
         </div>
       </div>
