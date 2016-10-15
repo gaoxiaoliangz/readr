70bd200bf5da241488fda19911c667cb13b0d253
liang <gaoxiaoliangz@gmail.com>
2016-10-15 15:25:59 +0800

    - react hot loader 可用

--- src/isomorphic/index.tsx
+++ src/isomorphic/index.tsx
@@ -3,14 +3,13 @@ import { render } from 'react-dom'
 import React from 'react'
 import { browserHistory, hashHistory, match } from 'react-router'
 import routes from '../isomorphic/routes'
-import syncRoutes from '../isomorphic/routes/index.sync'
+// import syncRoutes from '../isomorphic/routes/index.sync'
 import { syncHistoryWithStore } from 'react-router-redux'
 import configureStore from '../isomorphic/store/configureStore'
 import rootSaga from '../isomorphic/store/sagas'
 import helpers from '../isomorphic/helpers'
 import AppContainer from './containers/AppContainer'
 import Root from '../isomorphic/containers/Root'
-import Test from '../isomorphic/containers/Test'
 
 const routerHistory = helpers.isServerRoutingEnabled()
   ? browserHistory
@@ -22,76 +21,27 @@ const rootEle = document.getElementById('root')
 
 store.runSaga(rootSaga)
 
-// render(
-//   <AppContainer>
-//     <Test/>
-//   </AppContainer>,
-//   rootEle
-// )
-
-// if (module.hot) {
-//   module.hot.accept('./containers/Test', () => {
-//     const NextRoot = require('./containers/Test').default
-
-//     console.info('hot patching')
-//     render(
-//       <AppContainer>
-//         <NextRoot/>
-//       </AppContainer>,
-//       rootEle
-//     )
-//   })
-// }
-
-
-render(
-  <AppContainer>
-    <Root history={history} routes={syncRoutes} store={store} />
-  </AppContainer>,
-  rootEle
-)
-
-if (module.hot) {
-  console.info('hot patching')
-  module.hot.accept('./containers/Root', () => {
-    const NextRoot = require('./containers/Root').default
-
+const renderApp = routes => {
+  match({ history, routes }, (error, redirectLocation, renderProps) => {
     render(
       <AppContainer>
-        <NextRoot history={history} routes={syncRoutes} store={store} />
+        <Root
+          store={store}
+          renderProps={renderProps}
+          />
       </AppContainer>,
       rootEle
     )
   })
 }
 
-// match({ history, routes }, (error, redirectLocation, renderProps) => {
+if (module.hot) {
+  module.hot.accept('../isomorphic/routes', () => {
+    console.info('hot patching')
 
-//   console.log(renderProps);
+    const nextRoutes = require('../isomorphic/routes').default
+    renderApp(nextRoutes)
+  })
+}
 
-//   render(
-//     <Root
-//       store={store}
-//       routerProps={renderProps}
-//       />,
-//     rootEle
-//   )
-// })
-
-
-// if (module.hot) {
-//   module.hot.accept('./containers/Root2', () => {
-//     console.log('hehe');
-
-//     // const NextRoot = require('./containers/Root2').default
-//     // render(
-//     //   <AppContainer>
-//     //     <NextRoot
-//     //       store={store}
-//     //       renderProps={renderProps}
-//     //       />
-//     //   </AppContainer>,
-//     //   rootEle
-//     // )
-//   })
-// }
+renderApp(routes)
