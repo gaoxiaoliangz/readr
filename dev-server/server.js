57f6f346a7e584aee54b4b355d4c8868943901a9
liang <gaoxiaoliangz@gmail.com>
2016-10-15 22:59:18 +0800

    -

--- dev-server/server.js
+++ dev-server/server.js
@@ -1,15 +1,15 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('./webpack.config')
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(3000, 'localhost', (err, result) => {
   if (err) {
-    return console.log(err);
+    console.error(err)
+  } else {
+    console.info('Listening at http://localhost:3000/')
   }
-
-  console.log('Listening at http://localhost:3000/');
-});
+})
