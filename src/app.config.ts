ed16e43c2921877906eb6d39c62c260f2626a395
liang <gaoxiaoliangz@gmail.com>
2016-10-17 00:40:55 +0800

    - config

---
+++ src/app.config.ts
@@ -0,0 +1,15 @@
+// todo
+// 应用这个 config
+const appConfig = {
+  api: {
+    prefix: 'api'
+  },
+  database: {
+    host: 'mongodb://localhost:27017/',
+    name: 'readr'
+  },
+  siteUrl: 'http://readrweb.com',
+  port: 3000,
+}
+
+export default appConfig
--- src/isomorphic/constants.ts
+++ src/isomorphic/constants.ts
@@ -1,3 +1,7 @@
 // 全局变量
 export const ENABLE_SERVER_ROUTING = '__ENABLE_SERVER_ROUTING__'
 export const INITIAL_STATE = '__INITIAL_STATE__'
+
+// 其他常量
+export const DOUBAN_API_ROOT = 'https://api.douban.com/v2'
+export const GITHUB_API_ROOT = 'https://api.github.com'
