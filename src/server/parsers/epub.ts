87d0f73758fc83f2c5f9bc790b9d124a4ff8dee7
liang <gaoxiaoliangz@gmail.com>
2016-11-17 12:46:04 +0800

    修改 promise

--- src/server/parsers/epub.ts
+++ src/server/parsers/epub.ts
@@ -109,8 +109,6 @@ const getOpsRoot = opfPath => {
     if (opsRoot.match(/^\//)) {
       opsRoot = opsRoot.replace(/^\//, '')
     }
-    // 去掉 '/'
-    // opsRoot = opsRoot.substr(0, opsRoot.length - 1)
   }
   return opsRoot
 }
@@ -127,55 +125,55 @@ const xmlToJs = xml => {
   })
 }
 
-export function binaryParser(binaryFile) {
-  // todo: 添加 try catch
-  const zip = new nodeZip(binaryFile, { binary: true, base64: false, checkCRC32: true })
-  const containerXml = extractZipContent('META-INF/container.xml')(zip)
+export async function binaryParser(binaryFile) {
+  try {
+    const zip = new nodeZip(binaryFile, { binary: true, base64: false, checkCRC32: true })
+    const containerXml = extractZipContent('META-INF/container.xml')(zip)
 
-  return xmlToJs(containerXml).then(containerJSON => {
+    const containerJSON = await xmlToJs(containerXml)
     const opfPath = containerJSON.container.rootfiles[0].rootfile[0]['$']['full-path']
     const root = getOpsRoot(opfPath)
     const contentXml = extractZipContent(`${root}content.opf`)(zip)
     const tocXml = extractZipContent(`${root}toc.ncx`)(zip)
 
-    return xmlToJs(tocXml).then(tocJSON => {
-      const parsedToc = parseToc(tocJSON)
-      const flatToc = flattenToc(parsedToc)
+    const tocJSON = await xmlToJs(tocXml)
+    const parsedToc = parseToc(tocJSON)
+    const flatToc = flattenToc(parsedToc)
 
-      const content = _(flatToc)
-        .map(item => _.pick(item, ['src', 'fileSrc']))
-        .union()
-        .map(item => ({
-          src: item['src'],
-          markdown: resolveContent(item['fileSrc'])(zip)
-        }))
-        .value()
+    const content = _(flatToc)
+      .map(item => _.pick(item, ['src', 'fileSrc']))
+      .union()
+      .map(item => ({
+        src: item['src'],
+        markdown: resolveContent(item['fileSrc'])(zip)
+      }))
+      .value()
 
-      return xmlToJs(contentXml).then(contentJSON => {
-        const metadata = _.get(contentJSON, ['package', 'metadata'], [])
-        const title = _.get(metadata[0], ['dc:title', 0])
-        let author = _.get(metadata[0], ['dc:creator', 0])
+    const contentJSON = await xmlToJs(contentXml)
+    const metadata = _.get(contentJSON, ['package', 'metadata'], [])
+    const title = _.get(metadata[0], ['dc:title', 0])
+    let author = _.get(metadata[0], ['dc:creator', 0])
 
-        if (typeof author === 'object') {
-          author = _.get(author, ['_'])
-        }
+    if (typeof author === 'object') {
+      author = _.get(author, ['_'])
+    }
 
-        const publisher = _.get(metadata[0], ['dc:publisher', 0])
-        const meta = {
-          title,
-          author,
-          publisher
-        }
+    const publisher = _.get(metadata[0], ['dc:publisher', 0])
+    const meta = {
+      title,
+      author,
+      publisher
+    }
 
-        return ({
-          meta,
-          toc: parsedToc.map(item => _.omit(item, ['fileSrc'])),
-          flatToc: flatToc.map(item => _.omit(item, ['fileSrc'])),
-          content
-        })
-      })
+    return ({
+      meta,
+      toc: parsedToc.map(item => _.omit(item, ['fileSrc'])),
+      flatToc: flatToc.map(item => _.omit(item, ['fileSrc'])),
+      content
     })
-  })
+  } catch (error) {
+    return Promise.reject(error)
+  }
 }
 
 export default function parser(pathOrBinary, useBinary: boolean = false) {
