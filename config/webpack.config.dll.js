d8523fcf0b88f749f9d322669e9c2af4f85175a2
liang <gaoxiaoliangz@gmail.com>
2017-03-08 00:45:00 +0800

    mod npm scripts

--- config/webpack.config.dll.js
+++ config/webpack.config.dll.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: paths.buildStatic,
-    filename: '[name].dll.js',
+    filename: 'js/[name].dll.js',
     library: '__dll_[name]__'
   },
   plugins: [
