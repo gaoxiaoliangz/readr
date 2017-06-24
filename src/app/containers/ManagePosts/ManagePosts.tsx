457593aa6af9e33d7f72777fb5d3ac61fda167b5
liang <gaoxiaoliangz@gmail.com>
2017-06-24 13:23:14 +0800

    add loadmore to manageposts

--- src/app/containers/ManagePosts/ManagePosts.tsx
+++ src/app/containers/ManagePosts/ManagePosts.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { compose, graphql } from 'react-apollo'
+import _ from 'lodash'
 import { sendNotification } from '../../actions'
 import POSTS_QUERY from '../../graphql/Posts.gql'
 import withIndicator from '../../helpers/withIndicator'
 import { PostVisibility, PostCategories, PostStatus } from '../../../server/api/enums'
 import InfoTable from '../../components/InfoTable'
+import Button from '../../components/Button/Button'
 
 type Data = State.Apollo<{
   posts: Schema.Connection<{
@@ -33,12 +35,40 @@ type Data = State.Apollo<{
 interface OwnProps {
 }
 
+interface State {
+  postStatus: 'PUBLISHED' | 'UNPUBLISHED' | 'DRAFT' | ''
+}
+
 interface StateProps {
   sendNotification: typeof sendNotification
   data: Data
 }
 
-class ManagePosts extends Component<OwnProps & StateProps, {}> {
+class ManagePosts extends Component<OwnProps & StateProps, State> {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      postStatus: ''
+    }
+  }
+
+  _loadMore() {
+    const lastCursor = _.last(this.props.data.posts.edges).cursor
+    this.props.data.fetchMore({
+      variables: {
+        after: lastCursor
+      },
+      updateQuery: (previousResult: Data, { fetchMoreResult }: { fetchMoreResult: Data }) => {
+        const edges = [...previousResult.posts.edges, ...fetchMoreResult.posts.edges]
+        return _.merge({}, fetchMoreResult, {
+          posts: {
+            edges
+          }
+        })
+      }
+    })
+  }
 
   render() {
     const rows = this.props.data.posts.edges.map((edge, index) => {
@@ -56,6 +86,15 @@ class ManagePosts extends Component<OwnProps & StateProps, {}> {
           header={['标题', '路径', '状态']}
           rows={rows}
         />
+        {
+          this.props.data.posts.pageInfo.hasNextPage && (
+            <Button
+              onClick={() => { this._loadMore() }}
+              width={200}
+              color="blue"
+            >{this.props.data.loading ? '加载中 ...' : '更多'}</Button>
+          )
+        }
       </div>
     )
   }
@@ -64,7 +103,9 @@ class ManagePosts extends Component<OwnProps & StateProps, {}> {
 const withData = graphql(POSTS_QUERY, {
   options: (props: OwnProps) => {
     return {
-      variables: {}
+      variables: {
+        first: 5
+      }
     }
   }
 })
