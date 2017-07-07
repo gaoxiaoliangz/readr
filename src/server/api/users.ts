a05d929be8ef839d79fc199c76e3dc7834187e42
liang <gaoxiaoliangz@gmail.com>
2017-07-07 20:08:07 +0800

    signup api more user-friendly

--- src/server/api/users.ts
+++ src/server/api/users.ts
@@ -2,23 +2,46 @@ import _ from 'lodash'
 import { ROLES } from '../../app/constants'
 import { makeBasicAPIMethods, makeResult } from './utils'
 import dataProvider from '../models/dataProvider'
+import errors from '../errors'
 
 const userBasicAPI = makeBasicAPIMethods(dataProvider.User)
+
+type CreateUserConfig = {
+  email: string
+  password: string
+}
+async function createUser(object: CreateUserConfig) {
+  const queryEmailResult = await dataProvider.User.findOne({
+    email: object.email
+  })
+
+  if (queryEmailResult) {
+    return Promise.reject(new errors.ValidationError('邮箱已存在！'))
+  }
+
+  let defaultName = object.email.split('@')[0]
+  const queryUsernameResult = await dataProvider.User.findOne({
+    username: defaultName
+  })
+
+  if (queryUsernameResult) {
+    defaultName += Math.random().toFixed(3).substr(2)
+  }
+
+  return dataProvider.User.utils.save({
+    ...object,
+    ...{
+      role: ROLES.USER,
+      username: defaultName,
+      display_name: defaultName
+    }
+  })
+}
 
 const userAPI = {
   ...userBasicAPI,
   ...{
-    add(object, options) {
-      const defaultName = object.email.split('@')[0]
-      return dataProvider.User.utils.save({
-        ...object,
-        ...{
-          role: ROLES.USER,
-          username: defaultName,
-          display_name: defaultName
-        }
-      })
-    },
+    add: createUser,
     find(options) {
       const { id } = options
       return dataProvider.User.findById(id).exec(res => {
