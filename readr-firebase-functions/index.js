064e3dc91c28b1162a60f2521f55291b4bad4ec8
gaoxiaoliangz <gaoxiaoliangz@gmail.com>
2018-02-11 12:30:10 +0800

    add eslint

--- readr-firebase-functions/index.js
+++ readr-firebase-functions/index.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const gcs = require('@google-cloud/storage')();
-const mkdirp = require('mkdirp-promise');
-const os = require('os');
-const path = require('path');
-const fs = require('fs');
+const express = require('express')
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+// const gcs = require('@google-cloud/storage')()
+const mkdirp = require('mkdirp-promise')
+const os = require('os')
+const path = require('path')
 const _ = require('lodash')
 const epubParser = require('@gxl/epub-parser').default
 
-const app = express();
+const app = express()
 
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp(functions.config().firebase)
 
 const cors = (allowedOrigins) => (req, res, next) => {
   const origin = req.headers.origin
@@ -30,23 +29,23 @@ const handleNotFound = (req, res, next) => {
   })
 }
 
-app.use(cors('*'));
+app.use(cors('*'))
 
 const getBookFileById = (id) => {
   return admin.database().ref('files/' + id)
     .once('value')
     .then(data => {
       const { filename } = data.val()
-      const tempLocalFile = path.join(os.tmpdir(), filename);
-      const tempLocalDir = path.dirname(tempLocalFile);
+      const tempLocalFile = path.join(os.tmpdir(), filename)
+      const tempLocalDir = path.dirname(tempLocalFile)
 
       return mkdirp(tempLocalDir)
         .then(() => {
           // Download file from bucket.
-          return admin.storage().bucket().file(filename).download({ destination: tempLocalFile });
+          return admin.storage().bucket().file(filename).download({ destination: tempLocalFile })
         })
         .then(() => {
-          console.log('The file has been downloaded to', tempLocalFile);
+          console.log('The file has been downloaded to', tempLocalFile)
           // Delete the file
           // fs.unlinkSync(tempLocalFile);
           return epubParser(tempLocalFile).then(epub => {
@@ -60,7 +59,7 @@ const createRouter = () => {
   const router = express.Router()
   router.get('/book-files/:id', (req, res) => getBookFileById(req.params.id).then(json => {
     res.send(json)
-  }));
+  }))
 
   router.use(handleNotFound)
   return router
@@ -75,3 +74,7 @@ exports.app = functions.https.onRequest((req, res) => {
   console.log(`requesting ${req.url}`)
   return app(req, res)
 })
+
+// exports.epubToBook = functions.storage.object().onChange(event => {
+
+// })
