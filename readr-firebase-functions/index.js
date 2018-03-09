d4ab9c417decaac4bbbfd175a15deb56ce62bae6
gaoxiaoliangz <gaoxiaoliangz@gmail.com>
2018-03-09 12:46:05 +0800

    update

--- readr-firebase-functions/index.js
+++ readr-firebase-functions/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const gcs = require('@google-cloud/storage')()
-// const spawn = require('child-process-promise').spawn
 const mkdirp = require('mkdirp-promise')
 const os = require('os')
 const path = require('path')
